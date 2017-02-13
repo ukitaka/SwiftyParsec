1df70ccc6be79250065835ece11d7e578bfc7e53
ukitaka <yuki.takahashi.1126@gmail.com>
2017-02-13 22:30:50 +0900

    Add project

---
+++ Resources/SwiftyParsec.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyParsec.h
+//  SwiftyParsec
+//
+//  Created by Yuki Takahashi on 2017/02/13.
+//  Copyright © 2017年 waft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyParsec.
+FOUNDATION_EXPORT double SwiftyParsecVersionNumber;
+
+//! Project version string for SwiftyParsec.
+FOUNDATION_EXPORT const unsigned char SwiftyParsecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyParsec/PublicHeader.h>
+
+

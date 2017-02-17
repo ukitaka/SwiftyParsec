3f500f1d271a479eb057456f4e62044761243b93
ukitaka <yuki.takahashi.1126@gmail.com>
2017-02-17 22:02:14 +0900

    Add Examples

---
+++ Examples/Examples/Examples.h
@@ -0,0 +1,19 @@
+//
+//  Examples.h
+//  Examples
+//
+//  Created by Yuki Takahashi on 2017/02/17.
+//  Copyright © 2017年 waft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Examples.
+FOUNDATION_EXPORT double ExamplesVersionNumber;
+
+//! Project version string for Examples.
+FOUNDATION_EXPORT const unsigned char ExamplesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Examples/PublicHeader.h>
+
+

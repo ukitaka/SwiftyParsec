369730fe841781553bf47c330b7b916fd956f2c1
ukitaka <yuki.takahashi.1126@gmail.com>
2017-02-17 22:28:55 +0900

    Add URL Parser sample

---
+++ Examples/Examples/URLParser.swift
@@ -0,0 +1,31 @@
+//
+//  URLParser.swift
+//  Examples
+//
+//  Created by Yuki Takahashi on 2017/02/17.
+//  Copyright © 2017年 waft. All rights reserved.
+//
+
+import SwiftyParsec
+
+/// URL Parser
+/// see: https://tools.ietf.org/html/rfc1738#section-4
+final public class URLParser {
+    public enum URL {
+        case http
+        case ftp
+        case news
+        case nntp
+        case telnet
+        case gopher
+        case wais
+        case mailto
+        case file
+        case prospero
+        case other
+    }
+    
+    public func parseURL() -> Parser<URL> {
+        fatalError("not implemented")
+    }
+}
